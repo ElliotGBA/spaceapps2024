2729402f9ea230826daca7dceb790451c6807750
Malk Daboor <malk.daboor@dal.ca>
2024-10-05 16:04:57 -0300

    first page css done

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./scss/App.scss";
 function App() {
   return (
     <div className="App">
-      <AnimatedBackground animationName="starryNight" />
+      <AnimatedBackground animationName="cosmicDust" />
       <header className="App-header">
           Welcome Youniversers!
       </header>
@@ -16,10 +16,11 @@ function App() {
           Imagine this: You are a NASA Astronaut exploring the Milky Way in a spaceship. 
           You are tasked with exploring our solar system and getting information about it.
           If you answer the call, you will be enlightened beyond what you can imagine.
+          <h3>ARE YOU READY TO ACCEPT THE CHALLENGE?</h3>
         </p>
-        <h3>ARE YOU READY TO ACCEPT THE CHALLENGE?</h3>
-        <button>Yes!</button>
-        <button>Yes!</button>
+        
+        <motion.button whileHover={{ scale: 1.2 }}>Yes!</motion.button>
+        <motion.button whileHover={{ scale: 1.2 }}>Yes!</motion.button>
       </section>
       </main>
     </div>
