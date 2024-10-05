1ddf021f94213f8607a84a6242c4e5a7150481de
Truc Tran <ttran@cs.dal.ca>
2024-10-05 13:49:17 -0300

    fonts?

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import React from "react";
+import { motion } from "framer-motion";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Hello, World!
+      <header className="App-header">
+        <h1>
+          Welcome Youniversers!
+        </h1>
+      </header>
+      <main>
+      <section id="content">
+        <p>
+          Imagine this: You are a NASA Astronaut exploring the Milky Way in a spaceship. 
+          You are tasked with exploring our solar system and getting information about it.
+          If you answer the call, you will be enlightened beyond what you can imagine.
+        </p>
+        <h3>ARE YOU READY TO ACCEPT THE CHALLENGE?</h3>
+        <button>Yes!</button>
+        <button>Yes!</button>
+      </section>
+      </main>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div className="Home">
+            <header className="Home-header">
+                <section id="Home-banner">
+                    <h1 className="homeWelcome">Welcome Youniversers!</h1>
+                    /* Logo */?
+                </section>
+                
+            </header>
+        </div>
+    )
+}
+
+export default Home;
