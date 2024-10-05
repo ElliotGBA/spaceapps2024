0719c97b30d2b08d6b692efcdd5861113ad1d1fd
Michael Mroz <michaelmroz01111@gmail.com>
2024-10-05 13:46:33 -0300

    added react routes and a placeholder component

--- src/App.js
+++ src/App.js
@@ -6,12 +6,42 @@ import { CelestialBody } from './classes/CelestialBody';
 
 function App() {
 
-  const sun = new CelestialBody("Sun", 420, 69)
+  //const sun = new CelestialBody("Sun", 420, 69)
+/**
+ * Pages:
+ * 
+ * intro page
+ * homepage (orrery)
+ * interactive page
+ * learn more (planet)
+ * quiz page
+ * maybe (learn even more)
+ */
+
+  // temp component for routing, replace with actual ones:
+  const Placeholder = () => {
+    return(
+      <div>
+        This is a place holder :) 
+      </div>
+    )
+  }
+ 
 
   return (
-    
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          
+        {/* intro page:*/}<Route index path="/" element={<Placeholder/>}/> 
+          <Route path="/home" element={<Placeholder/>}/>
+          <Route path="simulation" element={<Placeholder/>}/>
+          <Route path="details:name" element={<Placeholder/>}/>
+          <Route path="quiz:name" element={<Placeholder/>}/>
+          {/*<Route path="extraInfo:name"/> */}
+          <Route path="*" element={<Placeholder/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 
