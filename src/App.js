3df75977f74774febb7bfcd54577f6a7d2ae9942
Elliot Boutilier-Anderson <elliotgba@gmail.com>
2024-10-05 14:03:51 -0300

    planets displayed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
-import "./classes/CelestialBody";
-
-import { CelestialBody } from './classes/CelestialBody';
+import Orrery from "./components/Orrery";
 
 function App() {
 
-  const sun = new CelestialBody("Sun", 420, 69)
-
   return (
     <div className="App">
-      
+
+      <main>
+        <Orrery />
+      </main>
+
     </div>
   );
 
--- src/classes/CelestialBody.js
+++ src/classes/CelestialBody.js
@@ -10,49 +10,6 @@ class CelestialBody {
         this.name = name;
         this.initialVelocityVector = initialVelocityVector;
     }
-
-    get mass() {
-        return this.mass;
-    }
-    set mass(newMass) {
-        this.mass = newMass;
-    }
-    get position() {
-        return this.position;
-    }
-    set position(newPosition) {
-        this.position = newPosition;
-    }
-    get velocity() {
-        return this.velocity;
-    }
-    set velocity(newVelocity) {
-        this.velocity = newVelocity;
-    }
-    get acceleration() {
-        return this.acceleration;
-    }
-    set acceleration(newAcceleration) {
-        this.acceleration = newAcceleration;
-    }
-    get radius() {
-        return this.radius;
-    }
-    set radius(newRadius) {
-        this.radius = newRadius;
-    }
-    get colour() {
-        return this.colour;
-    }
-    set colour(newColour) {
-        this.colour = newColour;
-    }
-    get initialVelocityVector() {
-        return this.initialVelocityVector;
-    }
-    set initialVelocityVector(newVelocityVector) {
-        this.initialVelocityVector = newVelocityVector;
-    }
     
 }
 
