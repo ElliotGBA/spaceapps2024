628a497085e7f8332901168859c15b3ac21b87e3
phuongwj <phamjulia9@gmail.com>
2024-10-05 12:57:36 -0300

    added jul.html and jul.js for juls purposes

---
+++ src/jul.js
@@ -0,0 +1,12 @@
+const planets = {
+    "Sun": "10",
+    "Mercury": "199",
+    "Venus": "299",
+    "Earth": "399",
+    "Mars": "499",
+    "Jupiter": "599",
+    "Saturn": "699",
+    "Uranus": "799",
+    "Neptune": "899",
+    "Pluto": "999"
+}
