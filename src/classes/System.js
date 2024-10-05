ed186c495c35e6e9e544746c6845d9007e538566
Michael Mroz <michaelmroz01111@gmail.com>
2024-10-05 12:48:36 -0300

    initialize System branch

---
+++ src/classes/System.js
@@ -0,0 +1,25 @@
+import { CelestialBody } from "./CelestialBody";
+
+/**
+ * System Class for containing CelestialBody objects
+ */
+class System {
+
+    constructor(centralBody) {
+        this.bodies = [];
+        this.centralBody = centralBody;
+    }
+
+    addBody(celestialBody) {
+        if (typeof celestialBody != CelestialBody)  {
+            return;
+        }
+
+        this.bodies = this.bodies.concat([body]);
+    }
+
+
+
+}
+
+export { System }
