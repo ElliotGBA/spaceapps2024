2962a8fe1bf028507d80a3d3707d7dc3cba2e36d
phuongwj <phamjulia9@gmail.com>
2024-10-05 13:06:57 -0300

    added few lines

--- src/jul.js
+++ src/jul.js
@@ -10,3 +10,37 @@ const planets = {
     "Neptune": "899",
     "Pluto": "999"
 }
+
+const fetchPlanetPosition = async (planet, startDate, endDate, timeStep) => {
+    const url = 'https://ssd.jpl.nasa.gov/api/horizons.api';
+    const requestBody = {
+        format: "json",
+        COMMAND: planets[planet],
+        OBJ_DATA: "YES",
+        MAKE_EPHEM: "YES",
+        EPHEM_TYPE: "VECTORS",
+        CENTER: "@sun",
+        START_TIME: startDate,
+        STOP_TIME: endDate,
+        STEP_SIZE: timeStep
+    };
+
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(requestBody)
+        });
+
+        if (!response.ok) {
+            throw new Error("Network response was not ok huhuhu");
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Fetch error: ", error);
+    }
+};
